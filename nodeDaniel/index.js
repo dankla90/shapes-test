eec7353d6258b1b033ee0926c5d806529a943639
dankla90 <bassboom@hotmail.com>
2022-02-16 11:39:32 +0100

    node daniel update

--- nodeDaniel/index.js
+++ nodeDaniel/index.js
@@ -2,20 +2,28 @@ import fs from 'fs'
 import rdf from 'rdf-ext';
 import ParserN3 from '@rdfjs/parser-n3'
 import SHACLValidator from 'rdf-validate-shacl'
+import { SolidNodeClient } from 'solid-node-client';
 
 
+import $rdf from 'rdflib'
+import {getSolidDataset} from '@inrupt/solid-client'
 
-//import $rdf from 'rdflib'
-//import {getSolidDataset} from '@inrupt/solid-client'
 
-/*
 async function loadFromPod(path) {
   const dataset = await getSolidDataset(path)
   return dataset
- // const parser = new ParserN3({ rdf })
- // return rdf.dataset().import(parser.import(dataset))
+  // const stream = fs.createReadStream(path)
+  // const parser = new ParserN3({ rdf })
+  // return rdf.dataset().import(parser.import(stream))
 }
-*/
+
+
+const client = new SolidNodeClient();
+await client.login({
+  idp : "https://solidcommunity.net", // e.g. https://solidcommunity.net
+  username : "mandalorian1337",
+  password : "WT99im66wt",  
+});
 
 
 
@@ -27,11 +35,11 @@ async function loadDataset (filePath) {
 
 async function main() {
 
-  //const path1 = 'https://martinclone.inrupt.net/public/test/dataset'
-  //const path2 = 'https://martinclone.inrupt.net/public/test/shape'
-  //const shapes = await loadFromPod(path1)
-  //const data = await loadFromPod(path2)
-
+  // const path2 = 'https://mandalorian1337.solidcommunity.net/lexitags/bookmarks' //'https://martinclone.inrupt.net/public/test/dataset'
+  // const path1 = 'https://martinclone.inrupt.net/public/test/shape'
+  // const shapes = await loadFromPod(path1)
+  // const data2 = await loadFromPod(path2)
+  // const data = await loadDataset(data2)
 
   const shapes = await loadDataset('my-shapes2.ttl')
   const data = await loadDataset('my-data.ttl')
