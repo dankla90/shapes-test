f6310e75638112959a51af777a7e9792a51e103b
dankla90 <43035752+dankla90@users.noreply.github.com>
2022-02-22 16:13:33 +0100

    updates!!

--- nodeDaniel/spotlight.py
+++ nodeDaniel/spotlight.py
@@ -1,42 +1,19 @@
 from collections import UserString
+from typing import Tuple
 from matplotlib.pyplot import annotate
+from pyparsing import null_debug_action
 import requests
 from bs4 import BeautifulSoup
 import bs4
 import rdflib
+import spotlight
 
 ## input = UserString
 
 response = requests.get("https://www.dbpedia-spotlight.org/api")
 print(response.status_code)
-annotate('test Bergen is a great city', 1)
+## annotate('test Bergen is a great city', Tuple[0.5, 0.7])
 
-
-
-url = input("Enter or Paste the URL: ")
-name = input("Save File As ? : ")
-url.strip()
-
-try:
-    print("Connecting To The Host .... " + url)
-    res=requests.get(url)
-    souped=bs4.BeautifulSoup(res.text,'lxml')
-        
-    for a in souped(["script","style","link","img","nav","footer","title","meta"]):
-        a.decompose()   
-
-    text = souped.get_text()
-    lines = (line.strip() for line in text.splitlines())
-    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-    txtcontent = '\n'.join(chunk for chunk in chunks if chunk)
-
-    fname=str(name)+".txt"
-    freader = open(fname, "w",encoding="utf-8")
-    freader.write("*****Document Generated Using Web Scraper By Ishan Sharma(github.com/ishandeveloper)*****\n\n")
-    freader.write(str(txtcontent))
-    freader.close()
-    print(fname + " File Successfully Created")  
-  
-except:
-    print("Oops! An error has occurred or maybe you just entered an Invalid URL :D")
-
+annotations = spotlight.annotate('https://www.dbpedia-spotlight.org/api',
+                                  'Your test text',
+                                  confidence=0.4, support=20)
