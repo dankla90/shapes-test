3ac4a0f1e77c335f64bf19837b35f828208df136
dankla90 <bassboom@hotmail.com>
2022-03-03 13:25:21 +0100

    update

--- nodeDaniel/spotlight2.py
+++ nodeDaniel/spotlight2.py
@@ -1,7 +1,4 @@
 import spotlight
-import matplotlib
-import requests
-from spotlight import annotate
 
 annotations = spotlight.annotate('https://api.dbpedia-spotlight.org/en/annotate',
 'Test about Bergen',confidence=0.4,support=20)
