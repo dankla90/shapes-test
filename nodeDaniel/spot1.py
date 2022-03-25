fa478a4f61393229d52aaaf903f2772653a8ad21
dankla90 <bassboom@hotmail.com>
2022-03-25 16:19:38 +0100

    cleaned!

--- nodeDaniel/spot1.py
+++ nodeDaniel/spot1.py
@@ -1,49 +1,44 @@
 import requests
 import bs4
 import spotlight 
+import sys
 
+def main():
 
-textcontent = "my test text about Bergen city in Norway"
-
-## THIS JUST TEST IF THE SITE IS UP AND SHOULD RETURN 200
-## response = requests.get("https://www.dbpedia-spotlight.org/api")
-## print(response.status_code)
-## annotations = spotlight.annotate('https://api.dbpedia-spotlight.org/en/annotate',
-##                                 textcontent,confidence=0.4,support=20)
-## print(annotations)
-
-url = input("Enter or Paste the URL: ")
-url.strip()
-
-response = requests.get(url)
-
-try:
-    print("Connecting To The Host .... " + url)
-    res=requests.get(url)
-    souped=bs4.BeautifulSoup(res.text,'lxml')
+    ## url = input("Enter or Paste the URL: ")
+    url = sys.argv[1:]
+    url = ' '.join(url)
+    url.strip()
     
-    ## FILTERING THE TEXT FROM WEBSITE      
-    for a in souped(["script","style","link","img","nav","footer","title","meta"]):
-        a.decompose()   
+    try:
+        res=requests.get(url)
+        souped=bs4.BeautifulSoup(res.text,'lxml')
+        
+        ## FILTERING THE TEXT FROM WEBSITE      
+        for a in souped(["script","style","link","img","nav","footer","title","meta"]):
+            a.decompose()   
 
-    text = souped.get_text()
-    lines = (line.strip() for line in text.splitlines())
-    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-    txtcontent = '\n'.join(chunk for chunk in chunks if chunk)
-    
-    ## ANNOTATION OF THE TEXT
-    annotations = spotlight.annotate('https://api.dbpedia-spotlight.org/en/annotate',
-                                 txtcontent,confidence=0.9,support=40)
-    
+        text = souped.get_text()
+        lines = (line.strip() for line in text.splitlines())
+        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+        txtcontent = '\n'.join(chunk for chunk in chunks if chunk)
+        
+        ## ANNOTATION OF THE TEXT
+        annotations = spotlight.annotate('https://api.dbpedia-spotlight.org/en/annotate',
+                                    txtcontent,confidence=0.9,support=40)
+        
 
-    annotationsUpdated = []
-    for dict_item in annotations:
-            item = (dict_item['URI'])
-            annotationsUpdated.append(item)
+        annotationsUpdated = []
+        for dict_item in annotations:
+                item = (dict_item['URI'])
+                annotationsUpdated.append(item)
 
-    annotationsUpdated = list(dict.fromkeys(annotationsUpdated))
-    ## print(annotations)
-    print(annotationsUpdated)
+        annotationsUpdated = list(dict.fromkeys(annotationsUpdated))
+        ## print(annotations)
+        ## print(annotationsUpdated)
+        return(annotationsUpdated)
 
-except:
-    print("Oops! An error has occurred or maybe you just entered an Invalid URL :D")
+    except:
+        return ("Oops! An error has occurred or maybe you just entered an Invalid URL :D")
+
+main()
